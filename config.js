7b376ce33db1a0a5c60137ed9f066d5527ca1bf7
Riya Banaulikar <riyabanaulikar@gmail.com>
2019-02-23 09:46:58 +0530

    db config

--- config.js
+++ config.js
@@ -3,13 +3,13 @@
 const config = module.exports;
 
 config.db = {
-  user: 'root',
-  password: '',
-  name: 'itgoa',
+  user: 'itgoadb',
+  password: 'it-#goa19',
+  name: 'itgoadb',
 };
 
 config.db.details = {
-  host: 'localhost',
+  host: 'itgoadb.cwcizbahiang.us-east-2.rds.amazonaws.com',
   port: 3306,
   dialect: 'mysql',
 };
